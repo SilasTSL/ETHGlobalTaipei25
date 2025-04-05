52d570aa760604c6eb23ad666987d74819c4a542
SilasTSL <silastaysl@gmail.com>
2025-04-05 14:28:11 +0800

    Connect mongo

--- recommendation/app.py
+++ recommendation/app.py
@@ -3,11 +3,17 @@ from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+from pymongo import MongoClient
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+# MongoDB connection
+mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
+client = MongoClient(mongo_uri)
+db = client['recommendation_db']
 
 @app.route('/api/recommendations', methods=['GET'])
 def get_recommendations():
